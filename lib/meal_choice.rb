82a240c64ecdea3a54eb31776c7d194f12ddcfc8
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-01-03 14:32:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,7 @@
 # Your code here
+protien = meat
 
-def meal_choice(veg1, veg2, protien = meat)
+def meal_choice(veg1, veg2)
   puts "What a nutritious meal!"
   puts "A plate of #{protien} with #{veg1} and #{veg2}."
 end 
